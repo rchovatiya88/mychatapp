ab4b85ff1ea69b5892bc01198d752b3d447d428e
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-11-14 16:25:39 -0500

    connect socket

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New User connected');
+
+socket.on('disconnect',() =>{
+    console.log('User was disconnected')
+})
+});
+
 server.listen(3000, () => {
     console.log('Server is up on port 3000');
 });  
