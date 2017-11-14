e64846270e5a77d45972ab4c4f0e13dab61e544a
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-11-14 15:39:58 -0500

    connect

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Make a Chat APP
 
 - server folder store all js code
 - public folder styles,html, client side js
- 
+- set up the port and push to github
+- connect it to glitch
--- server/server.js
+++ server/server.js
@@ -2,11 +2,9 @@ const path = require('path');
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
-
-app.listen(port,() => {
-    console.log(`Server is up on port ${port}`);
-});
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});  
