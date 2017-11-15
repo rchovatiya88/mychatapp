a60531124fb7f7a1a5a310b1809af3368593144a
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-11-15 18:35:04 -0500

    added geolocation

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage , generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 var server = http.createServer(app);
@@ -14,46 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User connected');
 
-
     // socket .emit from admin text Welcome to the chat app
-
-    // socket.emit('newMessage', {
-    //     from: 'Admin',
-    //     text: 'Welcome to the Chat App',
-    //     createdAt: new Date().getTime()
-    // });
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-
-
     // socket.broadcast.emit form the admin text new user joined.
-
-    // socket.broadcast.emit('newMessage', {
-    //     from:'Admin',
-    //     text:'New user joined',
-    //     createdAt: new Date().getTime()
-    // });
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
-
-    // // to user from a user
-
+    // to user from a user
     socket.on('createMessage',(message, callback)=> {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback('This is from the server');
     
     });
-    // socket.on('createMessage', (message) => {
-    //  console.log('createMessage', message);
-    //  io.emit('newMessage', {
-    //      from: message.from,
-    //      text: message.text,
-    //      createdAt: new Date().getTime()
-    //  });
-    // });
+
+
+    socket.on('createLocationMessage', (coords)=> {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    });
 
 socket.on('disconnect',() =>{
     console.log('User was disconnected')
-})
+});
 });
 
 server.listen(3000, () => {
