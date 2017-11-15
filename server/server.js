8154c40ce4ed32ed4bc7466adddde557f0c32f36
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-11-15 15:08:16 -0500

    create tests

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 var server = http.createServer(app);
@@ -13,29 +14,40 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User connected');
 
+
     // socket .emit from admin text Welcome to the chat app
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the Chat App',
-        createdAt: new Date().getTime()
-    });
+    // socket.emit('newMessage', {
+    //     from: 'Admin',
+    //     text: 'Welcome to the Chat App',
+    //     createdAt: new Date().getTime()
+    // });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
 
     // socket.broadcast.emit form the admin text new user joined.
 
-    socket.broadcast.emit('newMessage', {
-        from:'Admin',
-        text:'New user joined',
-        createdAt: new Date().getTime()
+    // socket.broadcast.emit('newMessage', {
+    //     from:'Admin',
+    //     text:'New user joined',
+    //     createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
+
+    // // to user from a user
+
+    socket.on('createMessage',(message)=> {
+        console.log('createMessage', message);
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
-    socket.on('createMessage', (message) => {
-     console.log('createMessage', message);
-     io.emit('newMessage', {
-         from: message.from,
-         text: message.text,
-         createdAt: new Date().getTime()
-     });
-    });
+    // socket.on('createMessage', (message) => {
+    //  console.log('createMessage', message);
+    //  io.emit('newMessage', {
+    //      from: message.from,
+    //      text: message.text,
+    //      createdAt: new Date().getTime()
+    //  });
+    // });
 
 socket.on('disconnect',() =>{
     console.log('User was disconnected')
