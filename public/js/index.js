59094325af7fc8c2fead6c01e6a621fe0595d58d
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-11-15 14:09:13 -0500

    two messages

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,7 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-    
-socket.emit ('createMessage', {
-        from:'ronak',
-        text: 'yup, that was me'
-    });
 });
-
-
 
 socket.on('disconnect',function () {
     console.log('Disconnected from server');
@@ -17,4 +10,4 @@ socket.on('disconnect',function () {
 
 socket.on('newMessage', function (message){
     console.log ('newMessage', message);
-})
+});
--- server/server.js
+++ server/server.js
@@ -13,14 +13,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User connected');
 
-    socket.emit('newMessage',{
-       from:'tom',
-       text: 'see you send',
-       createdAt:12213
-    });
      
     socket.on('createMessage', (message) => {
-        console.log('message created', message);
+     console.log('createMessage', message);
+     io.emit('newMessage', {
+         from: message.from,
+         text: message.text,
+         createdAt: new Date().getTime()
+     });
     });
 
 
