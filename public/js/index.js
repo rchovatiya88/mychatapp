1aa15a00db4cb8e7c263116b972c93ca721a5403
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-11-15 17:26:01 -0500

    add form messages and show incoming messages in borwser

--- public/js/index.js
+++ public/js/index.js
@@ -10,13 +10,10 @@ socket.on('disconnect',function () {
 
 socket.on('newMessage', function (message){
     console.log ('newMessage', message);
-});
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
 
-socket.emit('createMessage',{
-    from:'Frank',
-    text:'Hi'
-}, function (data) {
-    console.log('Got it', data);
+    jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e){
