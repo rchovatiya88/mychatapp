511f63aa7661076200ca6a6a5725eb8149e4d26b
Ronak Chovatiya <rchovatiya88@gmail.com>
2017-11-15 14:41:32 -0500

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,7 +13,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User connected');
 
-     
+    // socket .emit from admin text Welcome to the chat app
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the Chat App',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.broadcast.emit form the admin text new user joined.
+
+    socket.broadcast.emit('newMessage', {
+        from:'Admin',
+        text:'New user joined',
+        createdAt: new Date().getTime()
+    });
     socket.on('createMessage', (message) => {
      console.log('createMessage', message);
      io.emit('newMessage', {
@@ -22,9 +36,6 @@ io.on('connection', (socket) => {
          createdAt: new Date().getTime()
      });
     });
-
-
-
 
 socket.on('disconnect',() =>{
     console.log('User was disconnected')
